ab106ef5a9c7982ecebf0afa0e2218dbf808ccf2
dhrumilparmar <dhrumilparmar27481@gmail.com>
2026-05-22 10:17:24 +0530

    fixed new-request component

--- Travel_Management/src/app/new-request/new-request.component.ts
+++ Travel_Management/src/app/new-request/new-request.component.ts
@@ -22,6 +22,29 @@ export class NewRequestComponent {
     });
   }
 
+    onSubmit(): void {
+    if (this.requestForm.invalid) {
+      // this.errorMessage = "Please fill all required fields correctly.";
+      return;
+    }
+
+    //     const requestData = this.requestForm.value;
+    // // this.adminService.createEmployee(employeeData);
+    // console.log('Form Submitted (Service disabled):', requestData);
+    // this.adminService.createTravelReq(requestData).subscribe({
+    //   next: (response) => {
+    //     // We have to use optional chaining ?. or bracket notation because
+    //     // TypeScript doesn't know for sure if 'fullName' exists on the 'any' type.
+    //     this.successMessage = `Employee "${response?.fullName}" created successfully!`;
+    //     this.employeeForm.reset({ status: 'Active' });
+    //   },
+    //   error: (err) => {
+    //     this.errorMessage = 'Failed to create employee. Please try again.';
+    //   }
+    // });
+  }
+
+
 
 
 nextStep(): void {
